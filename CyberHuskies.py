cd8ae922de146448f9cd6ad8c2b5602717172ad9
nalogman <69798849+nalogman@users.noreply.github.com>
2024-02-03 22:50:02 -0800

    add api

--- CyberHuskies.py
+++ CyberHuskies.py
@@ -1,3 +1,22 @@
-#CyberHuskies 
+#CyberHuskies
 
-print("Hello World!")
+
+
+
+#Link to website for scannign documents
+#https://gretel.ai/blog/automate-detecting-sensitive-personally-identifiable-information-pii-with-gretel
+#https://www.makeuseof.com/python-create-document-scanner/
+
+
+
+
+
+
+from gretel_client import get_cloud_client
+
+
+# Connect to Gretel with your API key
+
+
+client = get_cloud_client(prefix="api", api_key="grtuc5c8f1506ce274fa026d375c0c63d7bf1ac9290d6f247b09160459dffd8916d5")
+client.install_packages()
